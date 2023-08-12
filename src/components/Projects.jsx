3b614beb83ba79de4f72dec28c4aa7c86f03e75a
ravellys <ravellyspyrrho@gmail.com>
2023-08-12 19:44:52 -0300

    create pageview logic

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,37 +1,53 @@
 import ProjectProfileCollection from '../ui-components/ProjectProfileCollection';
 import IrrigationSystemDesignService from '../services/irrigation_system_design_backend'
+import { useEffect, useState } from 'react';
+import { Button } from 'react-bootstrap';
+import PageView from '../domain/enum/PageView';
 
 const irrigationSystemDesignService = new IrrigationSystemDesignService();
 
-function Projects({user}) {
+function Projects({ user, setPageView }) {
+    const [projects, setProjects] = useState([]);
 
+    useEffect(() => {
+        setProjects(irrigationSystemDesignService.getProjects());
+    }, [projects])
 
-    const items = irrigationSystemDesignService.getProjects();
-    console.log(items)
     return (
+        <div>
+            <ProjectProfileCollection
+                items={projects}
+                overrideItems={({ item, index }) => ({
+                    overrides: {
+                        ProjectName: {
+                            children: item.name
+                        },
+                        UpdateBadge: {
+                            style: {
+                                cursor: "pointer"
+                            },
+                            onClick: () => alert(`update Badge ${item.id}`)
+                        },
+                        DeleteBadge: {
+                            style: {
+                                cursor: "pointer"
+                            },
+                            onClick: () => alert(`delete Badge ${item.id}`)
+                        }
+                    }
+                })}
+            />
+            <br />
+            <Button
+                onClick={() => {
+                    alert("create a New Project");
+                    setPageView(PageView.CREATE_NEW_PROJECT);
+                }}
+            >
+                Create a New Project
+            </Button>
 
-      <ProjectProfileCollection
-        items={items}
-        overrideItems={({item, index})=> ({
-            overrides: {
-                ProjectName: {
-                    children: item.name
-                },
-                UpdateBadge: {
-                    style: {
-                        cursor: "pointer"
-                    },
-                    onClick: () => alert(`update Badge ${item.id}`)
-                },
-                DeleteBadge: {
-                    style: {
-                        cursor: "pointer"
-                    },
-                    onClick: () => alert(`delete Badge ${item.id}`)
-                }
-            }
-        })} 
-      />
+        </div>
     )
 }
 
