0fa6ad126f07839fcd5c4f78953806d017e6805c
geanderson062 <83518241+geanderson-062@users.noreply.github.com>
2023-09-05 21:05:21 -0300

    menu buttons component

---
+++ src/components/Buttons/HomeButtons/index.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
+
+function HomeButtons() {
+  return (
+    <>
+      <div className="d-grid gap-2 col-6 mx-auto custom-button">
+        <Link
+          to="/Evapotranspiration"
+          className="btn btn-secondary custom-button"
+          type="button"
+        >
+          Evapotranspiration
+        </Link>
+        <Link
+          to="/CropCoefficient"
+          className="btn btn-secondary custom-button"
+          type="button"
+        >
+          CropCoefficient
+        </Link>
+        <Link
+          to="/PercenteShaded"
+          className="btn btn-secondary custom-button"
+          type="button"
+        >
+          PercenteShaded
+        </Link>
+        <Link
+          to="/SizeLines"
+          className="btn btn-secondary custom-button"
+          type="button"
+        >
+          SizeLines
+        </Link>
+        <Link
+          to="/Irrigation"
+          className="btn btn-secondary custom-button"
+          type="button"
+        >
+          Irrigation
+        </Link>
+        <Link
+          to="/IrrigationSystem"
+          className="btn btn-secondary custom-button"
+          type="button"
+        >
+          IrrigationSystem
+        </Link>
+      </div>
+    </>
+  );
+}
+
+export default HomeButtons;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,10 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import HomeButons from "../components/Buttons/HomeButtons";
 
 function Home() {
   return (
     <>
-      <div>Home</div>
-      <Link to="/CropCoefficient">CropCoefficient</Link>
-      <Link to="/Evapotranspiration">Evapotranspiration</Link>
-      <Link to="/IrrigationSystem">IrrigationSystem</Link>
-      <Link to="/PercenteShaded">PercenteShaded</Link>
-      <Link to="/SizeLines">SizeLines</Link>
-      <Link to="/Irrigation">Irrigation</Link>
+      <HomeButons />
     </>
   );
 }
