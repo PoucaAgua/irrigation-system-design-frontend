f33b111b1552b1d16e9a2cece7e362b8498beb98
geanderson062 <83518241+geanderson-062@users.noreply.github.com>
2023-09-08 18:54:17 -0300

    Update PercenteShaded.js

--- src/pages/PercenteShaded.js
+++ src/pages/PercenteShaded.js
@@ -1,8 +1,24 @@
 import React from "react";
 //import { Link } from "react-router-dom";
+import Header from "../components/Header";
+import TitlePercentShaded from "../components/Titles/Title_PercenteShaded";
+import PercenteShadedInputs from "../components/Inputs/PercentShaded_input";
+import ModalPW from "../components/Modals/Modal_pw";
+import ModalPS from "../components/Modals/Modal_ps";
 
-function PercenteShaded() {
-  return <h1 className="text-center">PercenteShaded</h1>;
+function PercenteShaded({ user, signOut }) {
+  return (
+    <>
+      <Header user={user} signOut={signOut} />
+
+      <TitlePercentShaded />
+      <PercenteShadedInputs />
+      <div className="d-flex justify-content-center">
+        <ModalPW />
+        <ModalPS />
+      </div>
+    </>
+  );
 }
 
 export default PercenteShaded;
