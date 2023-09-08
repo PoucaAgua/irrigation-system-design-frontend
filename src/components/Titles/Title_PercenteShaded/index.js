7d0a1fe211906ecd0b3d85bddbb03d82d9cd3459
geanderson062 <83518241+geanderson-062@users.noreply.github.com>
2023-09-08 18:54:37 -0300

    Create title

---
+++ src/components/Titles/Title_PercenteShaded/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class index extends Component {
+  render() {
+    return (
+      <h1 className="text-center" style={{ marginTop: 25 }}>
+        PercenteShaded
+      </h1>
+    );
+  }
+}
