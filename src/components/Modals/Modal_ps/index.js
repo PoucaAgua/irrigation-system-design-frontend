b0557304c6fe5056fb905603202143ab45e17f9c
geanderson062 <83518241+geanderson-062@users.noreply.github.com>
2023-09-12 13:13:39 -0300

    update modals

--- src/components/Modals/Modal_ps/index.js
+++ src/components/Modals/Modal_ps/index.js
@@ -1,81 +1,171 @@
 import React, { Component } from "react";
 
-export default class index extends Component {
+export default class Index extends Component {
   render() {
     return (
       <>
-        <button
-          type="button"
-          className="btn btn-secondary mr-3"
-          style={{ width: "150px", marginLeft: 14 }}
-          data-bs-toggle="modal"
-          data-bs-target="#CalculatePs"
-        >
-          Calculate Ps
-        </button>
+        <div className="dropdown">
+          <button
+            className="btn btn-secondary dropdown-toggle"
+            type="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+            style={{ width: "150px", marginLeft: 14 }}
+          >
+            Calculate Ps
+          </button>
+          <ul className="dropdown-menu">
+            <li>
+              <a
+                className="dropdown-item"
+                href="0"
+                data-bs-toggle="modal"
+                data-bs-target="#Canopy"
+              >
+                Plant Canopy Projection
+              </a>
+            </li>
+            <li>
+              <a
+                className="dropdown-item"
+                href="0"
+                data-bs-toggle="modal"
+                data-bs-target="#Strip"
+              >
+                Plant Strip Projection
+              </a>
+            </li>
+          </ul>
+        </div>
 
+        {/* Modal 1 */}
         <div
-          class="modal fade"
-          id="CalculatePs"
-          tabindex="-1"
-          aria-labelledby="exampleModalLabel"
+          className="modal fade"
+          id="Canopy"
+          tabIndex="-1"
+          aria-labelledby="ModalOneLabel"
           aria-hidden="true"
         >
-          <div class="modal-dialog">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h1 class="modal-title fs-5" id="exampleModalLabel">
-                  Percentage of shaded area - Ps
-                </h1>
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="ModalOneLabel">
+                  Percent Shaded Area By Plant Canopy Projection
+                </h5>
                 <button
                   type="button"
-                  class="btn-close"
+                  className="btn-close"
                   data-bs-dismiss="modal"
                   aria-label="Close"
                 ></button>
               </div>
-              <div class="modal-body">
-                <div className="d-flex flex-column align-items-center">
-                  <div className="form-group">
-                    <label htmlFor="input5" className="left-label">
-                      Shaded strip:
-                    </label>
-                    <input
-                      id="input5"
-                      type="text"
-                      className="form-control mb-3"
-                      style={{ width: "330px" }}
-                      placeholder="Ss (m)"
-                    />
-                  </div>
-
-                  <div className="form-group">
-                    <label htmlFor="input6" className="left-label">
-                      Crown projection diameter:
-                    </label>
-                    <input
-                      id="input6"
-                      type="text"
-                      className="form-control mb-3"
-                      style={{ width: "330px" }}
-                      placeholder="Dco (m)"
-                    />
-                  </div>
-                </div>
+              <div className="modal-body">
+                <label htmlFor="input5" className="left-label">
+                  Crown projection diameter:
+                </label>
+                <input
+                  id="input5"
+                  type="text"
+                  className="form-control mb-3"
+                  style={{ width: "330px" }}
+                  placeholder="Dco (m)"
+                />
+                <label htmlFor="input5" className="left-label">
+                  Spacing between plants in the row:
+                </label>
+                <input
+                  id="input5"
+                  type="text"
+                  className="form-control mb-3"
+                  style={{ width: "330px" }}
+                  placeholder="Sp (m)"
+                />
+                <label htmlFor="input5" className="left-label">
+                  Spacing between rows of plants:
+                </label>
+                <input
+                  id="input5"
+                  type="text"
+                  className="form-control mb-3"
+                  style={{ width: "330px" }}
+                  placeholder="Sr (m)"
+                />
               </div>
-              <div class="modal-footer">
+              <div className="modal-footer">
+                <button type="button" className="btn btn-secondary">
+                  Validate
+                </button>
+                <button type="button" className="btn btn-secondary">
+                  Calculate
+                </button>
                 <button
                   type="button"
-                  class="btn btn-danger"
+                  className="btn btn-danger"
                   data-bs-dismiss="modal"
                 >
                   Close
                 </button>
-                <button type="button" class="btn btn-secondary">
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Modal 2 */}
+        <div
+          className="modal fade"
+          id="Strip"
+          tabIndex="-1"
+          aria-labelledby="ModalTwoLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="ModalTwoLabel">
+                  Percent Shaded Area By Plant Strip Projection
+                </h5>
+                <button
+                  type="button"
+                  className="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div className="modal-body">
+                <div className="input mb-3">
+                  <label class="form-label">
+                    Spacing between rows of plants:
+                  </label>
+                  <input
+                    id="input5"
+                    type="text"
+                    className="form-control"
+                    style={{ width: "330px" }}
+                    placeholder="Sr (m)"
+                  />
+                  <label class="form-label">Shaded strip:</label>
+                  <input
+                    id="input5"
+                    type="text"
+                    className="form-control"
+                    style={{ width: "330px" }}
+                    placeholder="Ss (m)"
+                  />
+                </div>
+              </div>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-secondary">
                   Validate
                 </button>
-                <button type="button" class="btn btn-secondary">
+                <button type="button" className="btn btn-secondary">
                   Calculate
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-danger"
+                  data-bs-dismiss="modal"
+                >
+                  Close
                 </button>
               </div>
             </div>
--- src/components/Modals/Modal_pw/index.js
+++ src/components/Modals/Modal_pw/index.js
@@ -1,126 +1,345 @@
 import React, { Component } from "react";
-import RadioLateral from "../../Check_and_Radio/Lateral_lines_PW";
-import RadioIrrigaton from "../../Check_and_Radio/Irrigation_pw";
 
-export default class index extends Component {
+export default class Index extends Component {
   render() {
     return (
       <>
-        <button
-          type="button"
-          className="btn btn-secondary mr-3"
-          style={{ width: "150px", marginRight: 14 }}
-          data-bs-toggle="modal"
-          data-bs-target="#CalculatePw"
-        >
-          Calculate Pw
-        </button>
+        <div className="dropdown">
+          <button
+            className="btn btn-secondary dropdown-toggle"
+            type="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+            style={{ width: "150px", marginRight: 14 }}
+          >
+            Calculate Pw
+          </button>
+          <ul className="dropdown-menu">
+            <li>
+              <a
+                className="dropdown-item"
+                href="0"
+                data-bs-toggle="modal"
+                data-bs-target="#tree"
+              >
+                Irrigation by tree
+              </a>
+            </li>
+            <li>
+              <a
+                className="dropdown-item"
+                href="0"
+                data-bs-toggle="modal"
+                data-bs-target="#radius"
+              >
+                Twice saturated wetted radius
+              </a>
+            </li>
+            <li>
+              <a
+                className="dropdown-item"
+                href="0"
+                data-bs-toggle="modal"
+                data-bs-target="#strip"
+              >
+                Continuous strip
+              </a>
+            </li>
+          </ul>
+        </div>
 
+        {/*modal init*/}
         <div
-          class="modal fade"
-          id="CalculatePw"
-          tabindex="-1"
-          aria-labelledby="exampleModalLabel"
+          className="modal fade"
+          id="tree"
+          tabIndex="-1"
+          aria-labelledby="ModalOneLabel"
           aria-hidden="true"
         >
-          <div class="modal-dialog">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h1 class="modal-title fs-5" id="exampleModalLabel">
-                  Percentage of wetted area - Pw
-                </h1>
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="ModalOneLabel">
+                  Irrigation by tree
+                </h5>
                 <button
                   type="button"
-                  class="btn-close"
+                  className="btn-close"
                   data-bs-dismiss="modal"
                   aria-label="Close"
                 ></button>
               </div>
-              <div class="modal-body">
-                <RadioLateral />
-                <br />
-                <RadioIrrigaton />
-                <br />
-                <div className="d-flex flex-column align-items-center">
-                  <div className="form-group">
-                    <label htmlFor="input2" className="left-label">
-                      Hydraulic conductivity:
-                    </label>
-                    <input
-                      id="input2"
-                      type="text"
-                      className="form-control mb-3"
-                      style={{ width: "330px" }}
-                      placeholder="Ko (cm h-1)"
-                    />
-                  </div>
+              <div className="modal-body">
+                <label htmlFor="input5" className="left-label">
+                  Number of drippers per plant:
+                </label>
+                <input
+                  id="input5"
+                  type="text"
+                  className="form-control mb-3"
+                  style={{ width: "330px" }}
+                  placeholder="Np"
+                />
 
-                  <div className="form-group">
-                    <label htmlFor="input3" className="left-label">
-                      Exponential Model Parameter for Soil K:
-                    </label>
-                    <input
-                      id="input3"
-                      type="text"
-                      className="form-control mb-3"
-                      style={{ width: "330px" }}
-                      placeholder="conductivity - α"
-                    />
-                  </div>
+                <label htmlFor="input5" className="left-label">
+                  Spacing between plants in the row:
+                </label>
+                <input
+                  id="input5"
+                  type="text"
+                  className="form-control mb-3"
+                  style={{ width: "330px" }}
+                  placeholder="Sp (m)"
+                />
 
-                  <div className="form-group">
-                    <label htmlFor="input4" className="left-label">
-                      Dripper flow rate (cm3 h-1):
-                    </label>
-                    <input
-                      id="input4"
-                      type="text"
-                      className="form-control mb-3"
-                      style={{ width: "330px" }}
-                      placeholder="(cm3 h-1)"
-                    />
-                  </div>
+                <label htmlFor="input5" className="left-label">
+                  Spacing between rows of plants:
+                </label>
+                <input
+                  id="input5"
+                  type="text"
+                  className="form-control mb-3"
+                  style={{ width: "330px" }}
+                  placeholder="Sr (m)"
+                />
 
-                  <div className="form-group">
-                    <label htmlFor="input5" className="left-label">
-                      Number of drippers per plant:
-                    </label>
-                    <input
-                      id="input5"
-                      type="text"
-                      className="form-control mb-3"
-                      style={{ width: "330px" }}
-                      placeholder="Np"
-                    />
-                  </div>
+                <label htmlFor="input5" className="left-label">
+                  Maximum diameter wetted by the dripper, m:
+                </label>
+                <input
+                  id="input5"
+                  type="text"
+                  className="form-control mb-3"
+                  style={{ width: "330px" }}
+                  placeholder="Dw (m)"
+                />
 
-                  <div className="form-group">
-                    <label htmlFor="input6" className="left-label">
-                      Wetting depth:
-                    </label>
-                    <input
-                      id="input6"
-                      type="text"
-                      className="form-control mb-3"
-                      style={{ width: "330px" }}
-                      placeholder="Z (cm)"
-                    />
-                  </div>
-                </div>
+                <p>Dw Calculate.</p>
+                <label htmlFor="input5" className="left-label">
+                  Wetting depth:
+                </label>
+                <input
+                  id="input5"
+                  type="text"
+                  className="form-control mb-3"
+                  style={{ width: "330px" }}
+                  placeholder="Z (cm)"
+                />
+
+                <label htmlFor="input5" className="left-label">
+                  Q:
+                </label>
+                <input
+                  id="input5"
+                  type="text"
+                  className="form-control mb-3"
+                  style={{ width: "330px" }}
+                  placeholder="Q"
+                />
+
+                <label htmlFor="input5" className="left-label">
+                  Hydraulic conductivity of saturated soil:
+                </label>
+                <input
+                  id="input5"
+                  type="text"
+                  className="form-control mb-3"
+                  style={{ width: "330px" }}
+                  placeholder="cm h-1"
+                />
               </div>
-              <div class="modal-footer">
-                <button
-                  type="button"
-                  class="btn btn-danger"
-                  data-bs-dismiss="modal"
-                >
-                  Close
-                </button>
+              <div className="modal-footer">
                 <button type="button" class="btn btn-secondary">
                   Validate
                 </button>
                 <button type="button" class="btn btn-secondary">
                   Calculate
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-danger"
+                  data-bs-dismiss="modal"
+                >
+                  Close
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/*modal init*/}
+        <div
+          className="modal fade"
+          id="radius"
+          tabIndex="-1"
+          aria-labelledby="ModalOneLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="ModalOneLabel">
+                  Twice saturated wetted radius
+                </h5>
+                <button
+                  type="button"
+                  className="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div className="modal-body">
+                <label htmlFor="input5" className="left-label">
+                  Q:
+                </label>
+                <input
+                  id="input5"
+                  type="text"
+                  className="form-control mb-3"
+                  style={{ width: "330px" }}
+                  placeholder="Q"
+                />
+
+                <label htmlFor="input5" className="left-label">
+                  Hydraulic conductivity:
+                </label>
+                <input
+                  id="input5"
+                  type="text"
+                  className="form-control mb-3"
+                  style={{ width: "330px" }}
+                  placeholder="Ko (cm h-1)"
+                />
+
+                <label htmlFor="input5" className="left-label">
+                  Exponential model parameter for unsaturated soil hydraulic
+                  conductivity:
+                </label>
+                <input
+                  id="input5"
+                  type="text"
+                  className="form-control mb-3"
+                  style={{ width: "330px" }}
+                  placeholder="α"
+                />
+              </div>
+              <div className="modal-footer">
+                <button type="button" class="btn btn-secondary">
+                  Validate
+                </button>
+                <button type="button" class="btn btn-secondary">
+                  Calculate
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-danger"
+                  data-bs-dismiss="modal"
+                >
+                  Close
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/*modal init*/}
+        <div
+          className="modal fade"
+          id="strip"
+          tabIndex="-1"
+          aria-labelledby="ModalTwoLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="ModalTwoLabel">
+                  Continuous strip
+                </h5>
+                <button
+                  type="button"
+                  className="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div className="modal-body">
+                <label htmlFor="input5" className="left-label">
+                  Wetted area:
+                </label>
+                <input
+                  id="input5"
+                  type="text"
+                  className="form-control mb-3"
+                  style={{ width: "330px" }}
+                  placeholder="Aw = SP x SW , m²"
+                />
+                <label htmlFor="input5" className="left-label">
+                  Area occupied by the plant:
+                </label>
+                <input
+                  id="input5"
+                  type="text"
+                  className="form-control mb-3"
+                  style={{ width: "330px" }}
+                  placeholder="Ap = SP x SR, m²"
+                />
+                <p>Moistened area</p>
+                <label htmlFor="input5" className="left-label">
+                  Spacing between plants in the row:
+                </label>
+                <input
+                  id="input5"
+                  type="text"
+                  className="form-control mb-3"
+                  style={{ width: "330px" }}
+                  placeholder="Sp (m)"
+                />
+                <label htmlFor="input5" className="left-label">
+                  Wet Strip:
+                </label>
+                <input
+                  id="input5"
+                  type="text"
+                  className="form-control mb-3"
+                  style={{ width: "330px" }}
+                  placeholder="Sw (m²)"
+                />
+                <p>Area occupied plant</p>
+                <label htmlFor="input5" className="left-label">
+                  Spacing between rows of plants:
+                </label>
+                <input
+                  id="input5"
+                  type="text"
+                  className="form-control mb-3"
+                  style={{ width: "330px" }}
+                  placeholder="Sr (m)"
+                />
+                <label htmlFor="input5" className="left-label">
+                  Spacing between plants in the row:
+                </label>
+                <input
+                  id="input5"
+                  type="text"
+                  className="form-control mb-3"
+                  style={{ width: "330px" }}
+                  placeholder="Sp (m)"
+                />
+              </div>
+              <div className="modal-footer">
+                <button type="button" class="btn btn-secondary">
+                  Validate
+                </button>
+                <button type="button" class="btn btn-secondary">
+                  Calculate
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-danger"
+                  data-bs-dismiss="modal"
+                >
+                  Close
                 </button>
               </div>
             </div>
