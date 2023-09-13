3268c718ca72a1ff4bb19a34e87575539f4afbd5
geanderson062 <83518241+geanderson-062@users.noreply.github.com>
2023-09-13 18:43:53 -0300

    Create ps strip projection modal

---
+++ src/components/Modals/Modal_ps/strip_projection/index.js
@@ -0,0 +1,134 @@
+import React, { useState } from "react";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+
+import IrrigationSystemDesignService from "../../../../services/irrigation_system_design_backend";
+const irrigationSystemDesignService = new IrrigationSystemDesignService();
+
+function StripModal({ Strip, setStrip }) {
+  const [loadingCalculate, setLoadingCalculate] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [sr, setSr] = useState("");
+  const [ss, setSs] = useState("");
+  const [result, setResult] = useState(null);
+
+  const StripComponentOverrides = {
+    Sr: {
+      onChange: (event) => {
+        setSr(event.target.value);
+      },
+      type: "number",
+    },
+    Ss: {
+      onChange: (event) => {
+        setSs(event.target.value);
+      },
+      type: "number",
+    },
+
+    CalculateButton: {
+      onClick: async () => {
+        setLoadingCalculate(true);
+        const payload = {
+          space_between_rows: sr,
+          shaded_strip_plant: ss,
+        };
+
+        const response = await irrigationSystemDesignService.calculateStrip(
+          payload
+        );
+
+        console.log("API Response:", response);
+        setLoadingCalculate(false);
+      },
+    },
+
+    SaveButton: {
+      onClick: () => {
+        alert("SaveButton need be implemented....");
+      },
+    },
+    Strip: {
+      value: loadingCalculate ? "Loading..." : Strip,
+    },
+  };
+
+  const toggleModal = () => {
+    setShowModal(!showModal);
+  };
+
+  return (
+    <>
+      <div className="form-check" style={{ marginRight: 16 }}>
+        <input
+          type="radio"
+          className="form-check-input"
+          id="stripRadioButton"
+          name="modalRadio"
+          onChange={toggleModal}
+        />
+        <label className="form-check-label" htmlFor="stripRadioButton">
+          Strip Projection
+        </label>
+      </div>
+
+      <Modal show={showModal} onHide={toggleModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>
+            Percent Shaded Area By Plant Strip Projection
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="input mb-3">
+            <label className="form-label">
+              Spacing between rows of plants:
+            </label>
+            <input
+              id="input5"
+              type="text"
+              className="form-control"
+              style={{ width: "330px" }}
+              placeholder="Sr (m)"
+              value={sr}
+              onChange={(e) => setSr(e.target.value)}
+            />
+            <label className="form-label">Shaded strip:</label>
+            <input
+              id="input5"
+              type="text"
+              className="form-control"
+              style={{ width: "330px" }}
+              placeholder="Ss (m)"
+              value={ss}
+              onChange={(e) => setSs(e.target.value)}
+            />
+          </div>
+          {result !== null && (
+            <div>
+              <strong>Result:</strong> {result}
+            </div>
+          )}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            variant="success"
+            onClick={StripComponentOverrides.SaveButton.onClick}
+          >
+            Save
+          </Button>
+          <Button
+            variant="primary"
+            onClick={StripComponentOverrides.CalculateButton.onClick}
+          >
+            Calculate
+          </Button>
+          <Button variant="danger" onClick={toggleModal}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+export default StripModal;
