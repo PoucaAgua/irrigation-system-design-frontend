4d611c953471af6dc584358f85122172de13f9fb
geanderson062 <83518241+geanderson-062@users.noreply.github.com>
2023-09-15 21:49:51 -0300

    Connecting to the api

--- src/services/configuration.js
+++ src/services/configuration.js
@@ -6,6 +6,16 @@ class Configuration {
     "/dev/api/v1/irrigation/actual/atmospheric_params";
   ACTUAL_IRRIGATION_SOIL_PARAMS = "/dev/api/v1/irrigation/actual/soil_params";
   ACTUAL_IRRIGATION_MAX = "/dev/api/v1/irrigation/actual/max";
+  PS_STRIP_PROJECTION =
+    "/dev/api/v1/percent_shaded_area/plant_strip_projection";
+  PS_CANOPY_PROJECTION =
+    "/dev/api/v1/percent_shaded_area/plant_canopy_projection";
+  PW_WETTED_RADIUS =
+    "/dev/api/v1/percent_wetted_area/calculate_twice_saturated_wetted_radius";
+  PW_IRRIGATION_TREE =
+    "/dev/api/v1/percent_wetted_area/percent_wetted_area_irrigation_by_tree";
+  PW_CONTINUOUS_STRIP =
+    "/dev/api/v1/percent_wetted_area/percent_wetted_area_continuous_strip";
 }
 
 export default Configuration;
--- src/services/irrigation_system_design_backend.js
+++ src/services/irrigation_system_design_backend.js
@@ -1,16 +1,16 @@
 import Configuration from "./configuration";
-import { processResponse } from "../components/ErrorMensage/index";
+
 class IrrigationSystemDesignService {
   constructor() {
     this.config = new Configuration();
     this.URL = this.config.IRRIGATION_SYSTEM_DESIGN_BACKEND_URL;
     this.ETO_HARGRAVES_SAMANI =
       this.URL + this.config.ETO_HARGRAVES_SAMANI_PATH;
-    this.ACTUAL_IRRIGATION_ATMOSPHERIC =
-      this.URL + this.config.ACTUAL_IRRIGATION_ATMOSPHERIC;
-    this.ACTUAL_IRRIGATION_SOIL_PARAMS =
-      this.URL + this.config.ACTUAL_IRRIGATION_SOIL_PARAMS;
-    this.ACTUAL_IRRIGATION_MAX = this.URL + this.config.ACTUAL_IRRIGATION_MAX;
+    this.PS_STRIP = this.URL + this.config.PS_STRIP_PROJECTION;
+    this.PS_CANOPY = this.URL + this.config.PS_CANOPY_PROJECTION;
+    this.PW_WETTED_RADIUS = this.URL + this.config.PW_WETTED_RADIUS;
+    this.PW_IRRIGATION_TREE = this.URL + this.config.PW_IRRIGATION_TREE;
+    this.PW_CONTINUOUS_STRIP = this.URL + this.config.PW_CONTINUOUS_STRIP;
 
     this.projects = [
       {
@@ -58,8 +58,8 @@ class IrrigationSystemDesignService {
     return await response.json();
   }
 
-  async calculateAtmospheric(payload) {
-    const response = await fetch(this.ACTUAL_IRRIGATION_ATMOSPHERIC, {
+  async calculateStrip(payload) {
+    const response = await fetch(this.PS_STRIP, {
       method: "POST",
       headers: {
         accept: "application/json",
@@ -67,12 +67,6 @@ class IrrigationSystemDesignService {
       },
       body: JSON.stringify(payload),
     });
-
-    const errorMessage = await processResponse(response);
-
-    if (errorMessage) {
-      alert(errorMessage);
-    }
 
     if (!response.ok) {
       alert(response.value);
@@ -81,8 +75,8 @@ class IrrigationSystemDesignService {
     return await response.json();
   }
 
-  async calculateSoilParams(payload) {
-    const response = await fetch(this.ACTUAL_IRRIGATION_SOIL_PARAMS, {
+  async calculateCanopy(payload) {
+    const response = await fetch(this.PS_CANOPY, {
       method: "POST",
       headers: {
         accept: "application/json",
@@ -90,12 +84,6 @@ class IrrigationSystemDesignService {
       },
       body: JSON.stringify(payload),
     });
-
-    const errorMessage = await processResponse(response);
-
-    if (errorMessage) {
-      alert(errorMessage);
-    }
 
     if (!response.ok) {
       alert(response.value);
@@ -104,8 +92,8 @@ class IrrigationSystemDesignService {
     return await response.json();
   }
 
-  async calculateMaximum(payload) {
-    const response = await fetch(this.ACTUAL_IRRIGATION_MAX, {
+  async calculateRadius(payload) {
+    const response = await fetch(this.PW_WETTED_RADIUS, {
       method: "POST",
       headers: {
         accept: "application/json",
@@ -114,11 +102,39 @@ class IrrigationSystemDesignService {
       body: JSON.stringify(payload),
     });
 
-    const errorMessage = await processResponse(response);
-
-    if (errorMessage) {
-      alert(errorMessage);
+    if (!response.ok) {
+      alert(response.value);
     }
+
+    return await response.json();
+  }
+
+  async calculateTree(payload) {
+    const response = await fetch(this.PW_IRRIGATION_TREE, {
+      method: "POST",
+      headers: {
+        accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    });
+
+    if (!response.ok) {
+      alert(response.value);
+    }
+
+    return await response.json();
+  }
+
+  async calculatePwStrip(payload) {
+    const response = await fetch(this.PW_CONTINUOUS_STRIP, {
+      method: "POST",
+      headers: {
+        accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    });
 
     if (!response.ok) {
       alert(response.value);
