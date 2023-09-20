a2e45221746353fd524a692489fcfd02692e7d1d
geanderson062 <83518241+geanderson-062@users.noreply.github.com>
2023-09-20 20:27:56 -0300

    ps and pw cards

--- src/components/Titles/Titles_percent_shaded/Pw_area/index.js
+++ src/components/Titles/Titles_percent_shaded/Pw_area/index.js
@@ -3,8 +3,8 @@ import React, { Component } from "react";
 export default class index extends Component {
   render() {
     return (
-      <p className="fs-3 text-center" style={{ marginTop: 15 }}>
-        Percent Wetted Area - PW{" "}
+      <p className="fs-4 text-center" style={{ marginTop: 15 }}>
+        Percent Wetted - PW{" "}
       </p>
     );
   }
