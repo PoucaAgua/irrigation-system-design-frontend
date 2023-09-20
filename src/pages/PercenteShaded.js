4177b2b864b3cf4e135f5c6f26b7c57921a58495
geanderson062 <83518241+geanderson-062@users.noreply.github.com>
2023-09-20 17:30:42 -0300

    calculation component ps strip

--- src/pages/PercenteShaded.js
+++ src/pages/PercenteShaded.js
@@ -11,7 +11,7 @@ import PwTreeModalRender from "../components/Modals/Modal_pw/tree_wetted";
 import PwStripModalRender from "../components/Modals/Modal_pw/strip_wetted";
 import IrrigationSystemDesignService from "../services/irrigation_system_design_backend";
 import { createCanopyComponentOverrides } from "../components/AgronomicModule/PercentShaded/CanopyCalculation";
-
+import { createStripComponentOverrides } from "../components/AgronomicModule/PercentShaded/StripCalculation";
 const irrigationSystemDesignService = new IrrigationSystemDesignService();
 
 function PercenteShaded({
@@ -80,61 +80,18 @@ function PercenteShaded({
   };
 
   //PS strip
-  const StripComponentOverrides = {
-    Sr: {
-      onChange: (event) => {
-        setSr(event.target.value);
-        setValidationError("");
-      },
-      type: "number",
-    },
-    Ss: {
-      onChange: (event) => {
-        setSs(event.target.value);
-        setValidationError("");
-      },
-      type: "number",
-    },
-
-    CalculateButtonPsStrip: {
-      onClick: async () => {
-        if (!sr || !ss) {
-          setValidationError("Please fill in all fields.");
-          return;
-        }
-
-        setLoadingCalculate(true);
-        setValidationError("");
-
-        const payload = {
-          space_between_rows: sr,
-          shaded_strip_plant: ss,
-        };
-
-        try {
-          const response = await irrigationSystemDesignService.calculateStrip(
-            payload
-          );
-
-          console.log("API Response:", response);
-          setResultPsStrip(response.value);
-        } catch (error) {
-          console.error("Error calculating Strip:", error);
-        } finally {
-          setLoadingCalculate(false);
-        }
-      },
-    },
-
-    SaveButtonPsStrip: {
-      onClick: () => {
-        alert("SaveButton need to be implemented....");
-      },
-    },
-    Strip: {
-      value: loadingCalculate ? "Loading..." : Strip,
-    },
-  };
+  const StripComponentOverrides = createStripComponentOverrides({
+    sr,
+    ss,
+    setSr,
+    setSs,
+    setValidationError,
+    setLoadingCalculate,
+    setResultPsStrip,
+    Strip,
+    irrigationSystemDesignService,
+    loadingCalculate,
+  });
 
   const toggleModalStrip = () => {
     setShowModalPsStrip(!showModalPsStrip);
