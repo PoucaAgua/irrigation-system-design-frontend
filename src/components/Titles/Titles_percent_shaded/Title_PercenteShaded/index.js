1f2d61cc4e8329e2ee2c753b84de7b3e78bed335
geanderson062 <83518241+geanderson-062@users.noreply.github.com>
2023-09-26 19:36:24 -0300

    renaming percentShaded to PsAndPw

--- src/components/Titles/Titles_percent_shaded/Title_PercenteShaded/index.js
+++ src/components/Titles/Titles_percent_shaded/Title_PercenteShaded/index.js
@@ -4,7 +4,7 @@ export default class index extends Component {
   render() {
     return (
       <p className="text-center fs-1" style={{ marginTop: 25 }}>
-        PercenteShaded
+        Ps and Pw
       </p>
     );
   }
