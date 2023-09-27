3cee65cfa7c9afb8b808144790edf63b62b8f6c8
geanderson062 <83518241+geanderson-062@users.noreply.github.com>
2023-09-26 21:42:11 -0300

    displaying card modal when necessary

--- src/components/Inputs/PercentShaded_input/index.js
+++ src/components/Inputs/PercentShaded_input/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./style.css";
 
 function LabeledInputs({
@@ -61,6 +61,16 @@ function LabeledInputs({
     setIsInputPwDisabled(!isInputPwDisabled);
   };
 
+  const [showAdditionalFieldsPs, setShowAdditionalFieldsPs] = useState(false);
+  useEffect(() => {
+    setShowAdditionalFieldsPs(!isInputPsDisabled);
+  }, [isInputPsDisabled]);
+
+  const [showAdditionalFieldsPw, setShowAdditionalFieldsPw] = useState(false);
+  useEffect(() => {
+    setShowAdditionalFieldsPw(!isInputPwDisabled);
+  }, [isInputPwDisabled]);
+
   return (
     <div className="d-flex flex-column align-items-center">
       <div className="form-group">
@@ -116,148 +126,173 @@ function LabeledInputs({
         />
       </div>
 
-      <div className="form-group">
-        <label htmlFor="input1" className="left-label">
-          Spacing between rows of plants:
-        </label>
-        <input
-          id="input1"
-          type="text"
-          className="form-control mb-3 custom-input"
-          placeholder="Sr (m)"
-          value={sr}
-          onChange={(e) => setSr(e.target.value)}
-          disabled={isInputPsDisabled || isInputPwDisabled}
-        />
-      </div>
+      {showAdditionalFieldsPs && (
+        <>
+          {" "}
+          <div className="form-group">
+            <label htmlFor="input1" className="left-label">
+              Spacing between rows of plants:
+            </label>
+            <input
+              id="input1"
+              type="text"
+              className="form-control mb-3 custom-input"
+              placeholder="Sr (m)"
+              value={sr}
+              onChange={(e) => setSr(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="input2" className="left-label">
+              Spacing between plants in the row:
+            </label>
+            <input
+              id="input2"
+              type="text"
+              className="form-control mb-3 custom-input"
+              placeholder="Sp (m)"
+              value={sp}
+              onChange={(e) => setSp(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="input3" className="left-label">
+              Shaded strip:
+            </label>
+            <input
+              id="input3"
+              type="text"
+              className="form-control mb-3 custom-input"
+              placeholder="Ss (m)"
+              value={ss}
+              onChange={(e) => setSs(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="input3" className="left-label">
+              Crown projection diameter:
+            </label>
+            <input
+              id="input3"
+              type="text"
+              className="form-control mb-3 custom-input"
+              placeholder="Dco (m)"
+              value={dco}
+              onChange={(e) => setDco(e.target.value)}
+            />
+          </div>
+        </>
+      )}
 
-      <div className="form-group">
-        <label htmlFor="input2" className="left-label">
-          Spacing between plants in the row:
-        </label>
-        <input
-          id="input2"
-          type="text"
-          className="form-control mb-3 custom-input"
-          placeholder="Sp (m)"
-          value={sp}
-          onChange={(e) => setSp(e.target.value)}
-          disabled={isInputPsDisabled || isInputPwDisabled}
-        />
-      </div>
-
-      <div className="form-group">
-        <label htmlFor="input3" className="left-label">
-          Crown projection diameter:
-        </label>
-        <input
-          id="input3"
-          type="text"
-          className="form-control mb-3 custom-input"
-          placeholder="Sw (m²)"
-          value={sw}
-          onChange={(e) => StripPwComponentOverrides.Sw.onChange(e)}
-          disabled={isInputPsDisabled || isInputPwDisabled}
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="input3" className="left-label">
-          Shaded strip:
-        </label>
-        <input
-          id="input3"
-          type="text"
-          className="form-control mb-3 custom-input"
-          placeholder="Ss (m)"
-          value={ss}
-          onChange={(e) => setSs(e.target.value)}
-          disabled={isInputPsDisabled || isInputPwDisabled}
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="input3" className="left-label">
-          Crown projection diameter:
-        </label>
-        <input
-          id="input3"
-          type="text"
-          className="form-control mb-3 custom-input"
-          placeholder="Dco (m)"
-          value={dco}
-          onChange={(e) => setDco(e.target.value)}
-          disabled={isInputPsDisabled || isInputPwDisabled}
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="input3" className="left-label">
-          Number of drippers per plant:
-        </label>
-        <input
-          id="input3"
-          type="text"
-          className="form-control mb-3 custom-input"
-          placeholder="Np (m)"
-          value={np}
-          onChange={(e) => setNp(e.target.value)}
-          disabled={isInputPsDisabled || isInputPwDisabled}
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="input3" className="left-label">
-          Z:
-        </label>
-        <input
-          id="input3"
-          type="text"
-          className="form-control mb-3 custom-input"
-          placeholder="Z (m)"
-          value={z}
-          onChange={(e) => setZ(e.target.value)}
-          disabled={isInputPsDisabled || isInputPwDisabled}
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="input3" className="left-label">
-          Q:
-        </label>
-        <input
-          id="input3"
-          type="text"
-          className="form-control mb-3 custom-input"
-          placeholder="Q (m)"
-          value={q}
-          onChange={(e) => setQ(e.target.value)}
-          disabled={isInputPsDisabled || isInputPwDisabled}
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="input3" className="left-label">
-          Hydraulic conductivity:
-        </label>
-        <input
-          id="input3"
-          type="text"
-          className="form-control mb-3 custom-input"
-          placeholder="Ko (cm h-1)"
-          value={k0}
-          onChange={(e) => setK0(e.target.value)}
-          disabled={isInputPsDisabled || isInputPwDisabled}
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="input3" className="left-label">
-          Soil Water Conductivity Parameter:
-        </label>
-        <input
-          id="input3"
-          type="text"
-          className="form-control mb-3 custom-input"
-          placeholder="α"
-          value={alpha}
-          onChange={(e) => RadiusComponentOverrides.Alpha.onChange(e)}
-          disabled={isInputPsDisabled || isInputPwDisabled}
-        />
-      </div>
+      {showAdditionalFieldsPw && (
+        <>
+          {" "}
+          <div className="form-group">
+            <label htmlFor="input1" className="left-label">
+              Spacing between rows of plants:
+            </label>
+            <input
+              id="input1"
+              type="text"
+              className="form-control mb-3 custom-input"
+              placeholder="Sr (m)"
+              value={sr}
+              onChange={(e) => setSr(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="input2" className="left-label">
+              Spacing between plants in the row:
+            </label>
+            <input
+              id="input2"
+              type="text"
+              className="form-control mb-3 custom-input"
+              placeholder="Sp (m)"
+              value={sp}
+              onChange={(e) => setSp(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="input3" className="left-label">
+              Crown projection diameter:
+            </label>
+            <input
+              id="input3"
+              type="text"
+              className="form-control mb-3 custom-input"
+              placeholder="Sw (m²)"
+              value={sw}
+              onChange={(e) => StripPwComponentOverrides.Sw.onChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="input3" className="left-label">
+              Number of drippers per plant:
+            </label>
+            <input
+              id="input3"
+              type="text"
+              className="form-control mb-3 custom-input"
+              placeholder="Np (m)"
+              value={np}
+              onChange={(e) => setNp(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="input3" className="left-label">
+              Z:
+            </label>
+            <input
+              id="input3"
+              type="text"
+              className="form-control mb-3 custom-input"
+              placeholder="Z (m)"
+              value={z}
+              onChange={(e) => setZ(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="input3" className="left-label">
+              Q:
+            </label>
+            <input
+              id="input3"
+              type="text"
+              className="form-control mb-3 custom-input"
+              placeholder="Q (m)"
+              value={q}
+              onChange={(e) => setQ(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="input3" className="left-label">
+              Hydraulic conductivity:
+            </label>
+            <input
+              id="input3"
+              type="text"
+              className="form-control mb-3 custom-input"
+              placeholder="Ko (cm h-1)"
+              value={k0}
+              onChange={(e) => setK0(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="input3" className="left-label">
+              Soil Water Conductivity Parameter:
+            </label>
+            <input
+              id="input3"
+              type="text"
+              className="form-control mb-3 custom-input"
+              placeholder="α"
+              value={alpha}
+              onChange={(e) => RadiusComponentOverrides.Alpha.onChange(e)}
+            />
+          </div>
+        </>
+      )}
     </div>
   );
 }
