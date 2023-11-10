6f295f5dc62630623a9e632136641ef67b93b8e1
geanderson062 <83518241+geanderson-062@users.noreply.github.com>
2023-11-09 21:28:39 -0300

    changing file names

--- src/services/irrigation_system_design_backend.js
+++ src/services/irrigation_system_design_backend.js
@@ -1,26 +1,26 @@
 import Configuration from "./configuration";
-import EtoDesignService from "./EtoDesignService";
-import PSDesignService from "./PSDesignService";
-import PWDesignService from "./PWDesignService";
-import ActualIrrigationDesignService from "./ActualIrrigationDesignService";
+import EtoService from "./EtoService";
+import PSService from "./PSService";
+import PWService from "./PWService";
+import ActualIrrigationService from "./ActualIrrigationService";
 class IrrigationSystemDesignService {
   constructor() {
     this.config = new Configuration();
     this.URL = this.config.IRRIGATION_SYSTEM_DESIGN_BACKEND_URL;
-    this.etoService = new EtoDesignService(
+    this.etoService = new EtoService(
       (this.ETO_HARGRAVES_SAMANI =
         this.URL + this.config.ETO_HARGRAVES_SAMANI_PATH)
     );
-    this.psDesignService = new PSDesignService(
+    this.psService = new PSService(
       (this.PS_STRIP = this.URL + this.config.PS_STRIP_PROJECTION),
       (this.PS_CANOPY = this.URL + this.config.PS_CANOPY_PROJECTION)
     );
-    this.pwDesignService = new PWDesignService(
+    this.pwService = new PWService(
       (this.PW_WETTED_RADIUS = this.URL + this.config.PW_WETTED_RADIUS),
       (this.PW_IRRIGATION_TREE = this.URL + this.config.PW_IRRIGATION_TREE),
       (this.PW_CONTINUOUS_STRIP = this.URL + this.config.PW_CONTINUOUS_STRIP)
     );
-    this.actualirrigationService = new ActualIrrigationDesignService(
+    this.actualirrigationService = new ActualIrrigationService(
       (this.ACTUAL_IRRIGATION_ATMOSPHERIC =
         this.URL + this.config.ACTUAL_IRRIGATION_ATMOSPHERIC),
       (this.ACTUAL_IRRIGATION_SOIL_PARAMS =
@@ -75,23 +75,23 @@ class IrrigationSystemDesignService {
   }
 
   async calculateStrip(payload) {
-    return this.psDesignService.calculateStrip(payload);
+    return this.psService.calculateStrip(payload);
   }
 
   async calculateCanopy(payload) {
-    return this.psDesignService.calculateCanopy(payload);
+    return this.psService.calculateCanopy(payload);
   }
 
   async calculateRadius(payload) {
-    return this.pwDesignService.calculateRadius(payload);
+    return this.pwService.calculateRadius(payload);
   }
 
   async calculateTree(payload) {
-    return this.pwDesignService.calculateTree(payload);
+    return this.pwService.calculateTree(payload);
   }
 
   async calculatePwStrip(payload) {
-    return this.pwDesignService.calculatePwStrip(payload);
+    return this.pwService.calculatePwStrip(payload);
   }
 
   getProjects(params) {
