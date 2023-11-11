9b7dc4bb110a0d8be5453e5cbfe57f50f22340cb
geanderson062 <83518241+geanderson-062@users.noreply.github.com>
2023-11-11 16:41:49 -0300

    total irrigation page

--- src/pages/Irrigation_total.js
+++ src/pages/Irrigation_total.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "../components/Header/index";
+import CardTotal from "../components/TotalIrrigation/TotalIrrigationCard";
 
 function Irrigation({ user, signOut }) {
   return (
@@ -8,6 +9,7 @@ function Irrigation({ user, signOut }) {
       <h1 className="text-center" style={{ marginTop: 50 }}>
         Total Irrigation
       </h1>
+      <CardTotal />
     </>
   );
 }
