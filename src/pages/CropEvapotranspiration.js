dd7a3a056d0de776148f162a3eed266b7e1bb3a3
geanderson062 <83518241+geanderson-062@users.noreply.github.com>
2023-12-05 14:48:00 -0300

    create page

--- src/pages/CropEvapotranspiration.js
+++ src/pages/CropEvapotranspiration.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "../components/Header/index";
+import SelectorType from "../components/CropEvapotranspiration/CropEvapotranspirationSelector";
 
 function CropEvapotranspiration({ user, signOut }) {
   return (
@@ -8,6 +9,14 @@ function CropEvapotranspiration({ user, signOut }) {
       <h1 className="text-center fs-2" style={{ marginTop: 50 }}>
         CropEvapotranspiration
       </h1>
+      <div
+        className="d-flex flex-column align-items-center"
+        style={{ marginTop: 25 }}
+      >
+        <div className="form-group">
+          <SelectorType />
+        </div>
+      </div>
     </>
   );
 }
