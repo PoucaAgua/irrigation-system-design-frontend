cf41c0e6a493aea472dce333522e1725a20505be
geanderson062 <83518241+geanderson-062@users.noreply.github.com>
2024-01-17 10:48:42 -0300

    Update cards derivation line

--- src/components/SizingDerivationLine/SizingDerivationLineCards/index.js
+++ src/components/SizingDerivationLine/SizingDerivationLineCards/index.js
@@ -1,44 +1,96 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
 import { Container, Row, Col, Card } from "react-bootstrap";
+import ModalDiameter from "../SizingDerivationLineModals/Diameter";
+import ModalLoad from "../SizingDerivationLineModals/Load_loss";
+import { createSizingLoadComponentOverrides } from "../SizingDerivationLineCalculate/Load_loss";
+import { createSizingDiameterComponentOverrides } from "../SizingDerivationLineCalculate/Diameter";
+import IrrigationSystemDesignService from "../../../services/irrigation_system_design_backend";
+const irrigationSystemDesignService = new IrrigationSystemDesignService();
 
-const cardData = [
-  {
-    title: "Diameter ",
-    link: "/sizing/derivation_line",
-    imageSrc: "derivation-line-image-url.jpg",
-  },
-  {
-    title: "Load Loss",
-    link: "/sizing/lateral_line",
-    imageSrc: "lateral-line-image-url.jpg",
-  },
-];
+function SizingDerivationCard(Derivation, Load) {
+  const [loadingCalculate, setLoadingCalculate] = useState(false);
+  const [resultSizingLoad, setResultSizingLoad] = useState(null);
+  const [resultDiameter, setResultDiamater] = useState(null);
+  const [speedMax, setSpeedMax] = useState("");
+  const [demandFlow, setDemandFlow] = useState("");
+  const [diameterDerivation, setDiameterDerivation] = useState("");
+  const [noutputs, setNoutputs] = useState("");
+  const [flow, setFlow] = useState("");
+  const [lengthDerivation, setLengthDerivation] = useState("");
 
-function DerivationLineCards() {
+  const SizingDiameterComponentOverrides =
+    createSizingDiameterComponentOverrides({
+      irrigationSystemDesignService,
+      loadingCalculate,
+      setLoadingCalculate,
+      SizingDiameterComponentOverrides,
+      resultDiameter,
+      setResultDiamater,
+      speedMax,
+      setSpeedMax,
+      demandFlow,
+      setDemandFlow,
+    });
+
+  const SizingLoadComponentOverrides = createSizingLoadComponentOverrides({
+    irrigationSystemDesignService,
+    loadingCalculate,
+    setLoadingCalculate,
+    SizingLoadComponentOverrides,
+    resultSizingLoad,
+    setResultSizingLoad,
+    diameterDerivation,
+    setDiameterDerivation,
+    noutputs,
+    setNoutputs,
+    flow,
+    setFlow,
+    lengthDerivation,
+    setLengthDerivation,
+  });
+
   return (
     <Container style={{ marginTop: 50 }}>
       <Row>
-        {cardData.map((card, index) => (
-          <Col key={index} xs={12} sm={6} md={3}>
-            <Card className="mb-4">
-              <Card.Img variant="top" src={card.imageSrc} alt={card.title} />
-              <Card.Body>
-                <Card.Title>{card.title}</Card.Title>
-                <Link
-                  to={card.link}
-                  className="btn btn-secondary"
-                  style={{ width: 250, height: 40 }}
-                >
-                  Go to {card.title}
-                </Link>
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
+        <Col xs={12} sm={6} md={4}>
+          <Card className="mb-4">
+            <Card.Img variant="top" src={0} alt={0} />
+            <Card.Body>
+              <Card.Title>Diameter</Card.Title>
+              <ModalDiameter
+                SizingDiameterComponentOverrides={
+                  SizingDiameterComponentOverrides
+                }
+                resultDiameter={resultDiameter}
+                speedMax={speedMax}
+                setSpeedMax={setSpeedMax}
+                demandFlow={demandFlow}
+                setDemandFlow={setDemandFlow}
+              />
+            </Card.Body>
+          </Card>
+          <Card className="mb-4">
+            <Card.Img variant="top" src={0} alt={0} />
+            <Card.Body>
+              <Card.Title>Load Loss</Card.Title>
+              <ModalLoad
+                SizingLoadComponentOverrides={SizingLoadComponentOverrides}
+                resultSizingLoad={resultSizingLoad}
+                diameterDerivation={diameterDerivation}
+                setDiameterDerivation={setDiameterDerivation}
+                noutputs={noutputs}
+                setNoutputs={setNoutputs}
+                flow={flow}
+                setFlow={setFlow}
+                lengthDerivation={lengthDerivation}
+                setLengthDerivation={setLengthDerivation}
+              />
+            </Card.Body>
+          </Card>
+        </Col>
       </Row>
     </Container>
   );
 }
 
-export default DerivationLineCards;
+export default SizingDerivationCard;
