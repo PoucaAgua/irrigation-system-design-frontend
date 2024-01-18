b7db56bbb1694f905620c2b4c2816e4fdb4cdf29
geanderson062 <83518241+geanderson-062@users.noreply.github.com>
2024-01-18 12:11:33 -0300

    correct route header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./style.css";
 import IconWater from "../Icons/Icon_water";
+import { Link } from "react-router-dom";
 
 //user, signOut
 function Header({ user, signOut }) {
@@ -9,17 +10,18 @@ function Header({ user, signOut }) {
     <header className="p-2 mb-3 border-bottom bg-secondary">
       <div className="container">
         <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-          <h4
-            href="/"
+          <Link
+            to="/"
+            style={{ textDecoration: "none" }}
             className="d-flex align-items-center mb-2 mb-lg-0 link-body-emphasis text-white fs-4"
           >
             <IconWater />
             Pouca Água
-          </h4>
+          </Link>
 
           <div className="dropdown text-end ms-auto">
             <a
-              href="#"
+              href="0"
               className="d-block link-body-emphasis text-decoration-none dropdown-toggle"
               data-bs-toggle="dropdown"
               aria-expanded="false"
@@ -35,17 +37,17 @@ function Header({ user, signOut }) {
 
             <ul className="dropdown-menu text-small">
               <li>
-                <a className="dropdown-item" href="#">
+                <a className="dropdown-item" href="0">
                   New project...
                 </a>
               </li>
               <li>
-                <a className="dropdown-item" href="#">
+                <a className="dropdown-item" href="0">
                   Settings
                 </a>
               </li>
               <li>
-                <a className="dropdown-item" href="#">
+                <a className="dropdown-item" href="0">
                   Profile
                 </a>
               </li>
@@ -53,7 +55,7 @@ function Header({ user, signOut }) {
                 <hr className="dropdown-divider" />
               </li>
               <li>
-                <a className="dropdown-item" href="#" onClick={signOut}>
+                <a className="dropdown-item" href="0" onClick={signOut}>
                   Sign out
                 </a>
               </li>
