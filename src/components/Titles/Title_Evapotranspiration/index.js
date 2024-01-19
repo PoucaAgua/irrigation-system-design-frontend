eb1d84aca4347a7b68e98face3584d3acf94ca7e
geanderson-062 <83518241+geanderson-062@users.noreply.github.com>
2024-01-19 12:22:55 -0300

    Title Evapotranspiration

---
+++ src/components/Titles/Title_Evapotranspiration/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+export default class index extends Component {
+  render() {
+    return (
+      <p
+        className="fs-1 text-center"
+        style={{ marginTop: "3%", marginBottom: "3%" }}
+      >
+        Evapotranspiration
+      </p>
+    );
+  }
+}
