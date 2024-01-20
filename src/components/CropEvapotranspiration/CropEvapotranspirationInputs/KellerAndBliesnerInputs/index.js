34d7e7ed952760bcea345968f3ae0846cd2cf387
geanderson-062 <83518241+geanderson-062@users.noreply.github.com>
2024-01-19 23:20:50 -0300

    CropEvapotranspiration result

--- src/components/CropEvapotranspiration/CropEvapotranspirationInputs/KellerAndBliesnerInputs/index.js
+++ src/components/CropEvapotranspiration/CropEvapotranspirationInputs/KellerAndBliesnerInputs/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ResultCard from "../../CropEvapotranspirationCardResult";
 
 function Bernardo({
@@ -17,6 +17,11 @@ function Bernardo({
   setResultCropEvapotranspiration,
   KellerAndBliesnerComponentOverrides,
 }) {
+  const [calculated, setCalculated] = useState(false);
+  const handleCalculate = () => {
+    KellerAndBliesnerComponentOverrides.CalculateButtonKellerAndBliesner.onClick();
+    setCalculated(true);
+  };
   return (
     <>
       <p className="text-center fs-3" style={{ marginTop: 50 }}>
@@ -79,15 +84,17 @@ function Bernardo({
         <button
           class="btn btn-secondary"
           type="button"
-          onClick={
-            KellerAndBliesnerComponentOverrides.CalculateButtonKellerAndBliesner
-              .onClick
-          }
+          onClick={handleCalculate}
         >
           Calculate
         </button>
       </div>
-      <ResultCard resultCropEvapotranspiration={resultCropEvapotranspiration} />
+      <br />
+      {calculated && (
+        <ResultCard
+          resultCropEvapotranspiration={resultCropEvapotranspiration}
+        />
+      )}
     </>
   );
 }
