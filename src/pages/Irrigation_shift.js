7c74864931eccfe7eb6082439a8d0c060d501b5e
geanderson-062 <83518241+geanderson-062@users.noreply.github.com>
2024-01-31 12:17:37 -0300

    rename page

--- src/pages/Irrigation_shift.js
+++ src/pages/Irrigation_shift.js
@@ -2,7 +2,7 @@ import React from "react";
 import CardShift from "../components/Irrigation/ShiftIrrigation/IrrigationShiftCards";
 import TitleShiftIrrigation from "../components/Titles/Titles_Irrigation/Title_Irrigation_shift";
 
-function Irrigation() {
+function IrrigationShift() {
   return (
     <>
       <TitleShiftIrrigation />
@@ -11,4 +11,4 @@ function Irrigation() {
   );
 }
 
-export default Irrigation;
+export default IrrigationShift;
