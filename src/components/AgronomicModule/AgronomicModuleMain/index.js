9552420e9ab8edb01fd145c52bec19b323bc593f
geanderson-062 <83518241+geanderson-062@users.noreply.github.com>
2024-02-16 16:27:32 -0300

    system next component ready

--- src/components/AgronomicModule/AgronomicModuleMain/index.js
+++ src/components/AgronomicModule/AgronomicModuleMain/index.js
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Button } from "react-bootstrap";
 import SoilAndWaterDataInputs from "../SoilAndWaterDataInputs";
 import SoilAndWaterDataTitle from "../SoildAndWaterDateTitle";
 import ModalCEe from "../SoilAndWaterDateModals/ModalCEe/Index";
 import ModalF from "../SoilAndWaterDateModals/ModalF";
+import TestComponent from "../TestComponent";
 
 function AgronomicModuleMain() {
   const [soilCapacity, setSoilCapacity] = useState("");
@@ -15,47 +17,111 @@ function AgronomicModuleMain() {
   const [crop, setCrop] = useState("");
   const [CEe, setCEe] = useState("");
   const [f, setF] = useState("");
+  const [showContinueButton, setShowContinueButton] = useState(false);
+
+  useEffect(() => {
+    const allFieldsFilled =
+      soilCapacity !== "" &&
+      soilPermanent !== "" &&
+      soilConsumption !== "" &&
+      effectiveDepth !== "" &&
+      electricalSaturation !== "" &&
+      electricalIrrigation !== "" &&
+      group !== "" &&
+      crop !== "" &&
+      CEe !== "" &&
+      f !== "";
+
+    setShowContinueButton(allFieldsFilled);
+  }, [
+    soilCapacity,
+    soilPermanent,
+    soilConsumption,
+    effectiveDepth,
+    electricalSaturation,
+    electricalIrrigation,
+    group,
+    crop,
+    CEe,
+    f,
+  ]);
+
+  const [showNextComponent, setShowNextComponent] = useState(false);
+  const handleContinue = () => {
+    setSoilCapacity("");
+    setSoilPermanent("");
+    setSoilConsumption("");
+    setEffectiveDepth("");
+    setElectricalSaturation("");
+    setElectricalIrrigation("");
+    setGroup("");
+    setCrop("");
+    setCEe("");
+    setF("");
+    setShowContinueButton(false);
+    setShowNextComponent(true);
+  };
 
   return (
     <>
-      <div className="container">
-        <SoilAndWaterDataTitle />
-        <SoilAndWaterDataInputs
-          soilCapacity={soilCapacity}
-          setSoilCapacity={setSoilCapacity}
-          soilPermanent={soilPermanent}
-          setSoilPermanent={setSoilPermanent}
-          soilConsumption={soilConsumption}
-          setSoilConsumption={setSoilConsumption}
-          effectiveDepth={effectiveDepth}
-          setEffectiveDepth={setEffectiveDepth}
-          electricalSaturation={electricalSaturation}
-          setElectricalSaturation={setElectricalSaturation}
-          electricalIrrigation={electricalIrrigation}
-          setElectricalIrrigation={setElectricalIrrigation}
-        />
-        <div
-          className="text-center"
-          style={{ alignItems: "center", marginTop: 15, marginBottom: 15 }}
-        >
-          <ModalCEe
-            group={group}
-            setGroup={setGroup}
-            crop={crop}
-            setCrop={setCrop}
-            CEe={CEe}
-            setCEe={setCEe}
+      {!showNextComponent && (
+        <div className="container">
+          <SoilAndWaterDataTitle />
+          <SoilAndWaterDataInputs
+            soilCapacity={soilCapacity}
+            setSoilCapacity={setSoilCapacity}
+            soilPermanent={soilPermanent}
+            setSoilPermanent={setSoilPermanent}
+            soilConsumption={soilConsumption}
+            setSoilConsumption={setSoilConsumption}
+            effectiveDepth={effectiveDepth}
+            setEffectiveDepth={setEffectiveDepth}
+            electricalSaturation={electricalSaturation}
+            setElectricalSaturation={setElectricalSaturation}
+            electricalIrrigation={electricalIrrigation}
+            setElectricalIrrigation={setElectricalIrrigation}
           />
-          <ModalF
-            group={group}
-            setGroup={setGroup}
-            crop={crop}
-            setCrop={setCrop}
-            f={f}
-            setF={setF}
-          />
+          <div
+            className="text-center"
+            style={{ alignItems: "center", marginTop: 15, marginBottom: 15 }}
+          >
+            <ModalCEe
+              group={group}
+              setGroup={setGroup}
+              crop={crop}
+              setCrop={setCrop}
+              CEe={CEe}
+              setCEe={setCEe}
+            />
+            <ModalF
+              group={group}
+              setGroup={setGroup}
+              crop={crop}
+              setCrop={setCrop}
+              f={f}
+              setF={setF}
+            />
+          </div>
         </div>
-      </div>
+      )}
+      {showContinueButton && (
+        <div
+          style={{ display: "flex", justifyContent: "center", marginTop: 15 }}
+        >
+          <Button
+            onClick={handleContinue}
+            style={{
+              width: 220,
+              height: 40,
+            }}
+            variant="secondary"
+            className="text-center"
+          >
+            Continuar
+          </Button>
+        </div>
+      )}
+      {showNextComponent && <TestComponent />}
     </>
   );
 }
---
+++ src/components/AgronomicModule/TestComponent/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TestComponent() {
+  
+    return (
+      <div>Component ready</div>
+    )
+  }
+export default TestComponent;
